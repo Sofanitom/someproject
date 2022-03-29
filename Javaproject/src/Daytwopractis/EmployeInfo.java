2ffa9addabebf140eadcad458bbdb272fe030048
sofanitom <sofanitom@gmail.com>
2022-03-29 11:42:39 -0400

    first java github

---
+++ Javaproject/src/Daytwopractis/EmployeInfo.java
@@ -0,0 +1,33 @@
+package Daytwopractis;
+
+public class EmployeInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String empFirstname = "Sofia";	
+	String empLastname = "Mihretu";
+	
+	int empAge = 21;
+	
+	double empSalary = 5250;
+	
+	double empBonus = 300;
+	
+	double totalSalary = empSalary + empBonus;
+	
+	
+		System.out.println("Employe full name:" + empFirstname + " " + empLastname);
+		
+		System.out.println(
+				
+			"Employe age:" + empAge);
+		System.out.println("Employe salary:" + empSalary);
+		System.out.println("Employe bonus:" + empBonus);
+		System.out.println("Employe total salary:" + totalSalary);
+		
+		
+		
+	}
+
+}
---
+++ Javaproject/src/Daytwopractis/EmployeeInfo2.java
@@ -0,0 +1,41 @@
+package Daytwopractis;
+
+import java.util.Scanner;
+
+public class EmployeeInfo2 {
+// TODO Auto-generated ,ethod stub
+	
+	public static void main(String[] args) {
+		
+	
+	Scanner sc = new Scanner(System.in);	
+	
+	
+		System.out.println("please enter your full name:");
+	String empName = sc.next();
+	
+	System.out.println("please enter your age:");
+	int empAge = sc.nextInt();
+	
+	System.out.println("please enter your salary:");
+	double empSalary = sc.nextDouble();
+	
+	System.out.println("please enter your bonus");
+	double empBonus = sc.nextDouble();
+	
+	System.out.println("pls enter the empgender");
+	String str=sc.next();
+	char ch=str.charAt(0);
+	double totalSalary = empSalary + empBonus;
+	
+	// To print out the result on the console we will use this commands
+	
+	System.out.println("Employee Name: " + empName);
+	System.out.println("Employee Age: " + empAge);
+	System.out.println("Employee Salary: " + empSalary);
+	System.out.println("Employee Bonus: " + empBonus);
+	System.out.println("Employee Total Salary: " + totalSalary);
+    System.out.println("Employe Gender:"+ ch);
+
+   
+}}
